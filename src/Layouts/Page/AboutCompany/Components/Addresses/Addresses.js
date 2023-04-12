a61ba64d0b28eb7721545fa72d9b7b92e972d7c8
XGnarX <130073880+XGnarX@users.noreply.github.com>
2023-04-12 20:57:18 +0000

    блок обводка цвет

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,16 +1,28 @@
 import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
+
+import styled from "styled-components";
+
+const Address = styled.div`
+    border-radius: 24px;
+    border: solid #E2E8F0 1px;
+    padding: 24px;
+`;
+
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
+        <Address className='d-inline-flex flex-column gap-2 col justify-content-between'>
+            <div>
+            <div className='title'>Адреса заведений</div>
+            <div className='d-flex flex-column gap-3 mt-4'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
             </div>
-        </div>
+            </div>
+                <SecondaryButton>Добавить филиал</SecondaryButton>
+            
+        </Address>
     );
 }
 
