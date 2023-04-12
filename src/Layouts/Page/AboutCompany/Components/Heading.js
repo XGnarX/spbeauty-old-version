862d7d5748c6c6cc7be2bd57b20ffbb56c33b383
XGnarX <130073880+XGnarX@users.noreply.github.com>
2023-04-12 21:33:18 +0000

    тип организации и работа с полями ввода

--- src/Layouts/Page/AboutCompany/Components/Heading.js
+++ src/Layouts/Page/AboutCompany/Components/Heading.js
@@ -1,5 +1,4 @@
 import PrimaryButton from "../../../../Components/PrimaryButton";
-import SecondaryButton from "../../../../Components/SecondaryButton";
 function Heading() {
     return (
         <div className='d-flex flex-row justify-content-between mt-5'>
