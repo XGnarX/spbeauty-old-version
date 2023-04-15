5dbac3caeb9c10540cde3e961a3146b7313251f8
XGnarX <130073880+XGnarX@users.noreply.github.com>
2023-04-15 13:58:53 +0000

    поправил адреса расстояния

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -5,7 +5,7 @@ import ListElement from "./ListElement";
 import styled from "styled-components";
 
 const Address = styled.div`
-    border-radius: 8px;
+    border-radius: 24px;
     border: solid #E2E8F0 1px;
     padding: 24px;
 `;
@@ -13,9 +13,9 @@ const Address = styled.div`
 function Addresses() {
     return (
         <Address className='d-inline-flex flex-column gap-2 col justify-content-between me-5'>
-            <div>
+            <div className='d-flex flex-column gap-3'>
             <div className='title'>Адреса заведений</div>
-            <div className='d-flex flex-column gap-3 mt-4'>
+            <div className='d-flex flex-column gap-4 mt-4'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
             </div>
